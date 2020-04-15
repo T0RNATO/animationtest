162bbdf884720dcaa0a918c5d96b1973a21f7464
T0RNATO <62496453+T0RNATO@users.noreply.github.com>
2020-04-15 17:30:53 +1000

    Update anitest.js

--- anitest.js
+++ anitest.js
@@ -1,4 +1,4 @@
-var tcont = "<tr width=300 height=50></tr>"; //putting the stuff in the table
+var tcont = "<tr width=1300 height=50></tr>"; //putting the stuff in the table
 var tcontent = "<td width=50 height=50><button></button></td>"; //putting the stuff in the trs
 var ts = document.getElementsByTagName("tr");
 var done = [""];
@@ -8,10 +8,10 @@ var i = -1;
 var buttons = document.getElementsByTagName("button");
 document.getElementsByTagName("table")[0].innerHTML = tcont.repeat(14); //also putting the sutff in the table (14 collumns)
 for (w = 0; w < ts.length; w++) { //also putting the stuff in the trs
-  ts[w].innerHTML = tcontent.repeat(18); //(18 rows)
+  ts[w].innerHTML = tcontent.repeat(26); //(26 rows)
 }
 function vanish() {
-  obj = Math.floor(Math.random() * 252); //random number between 0 and 14 * 18, or 252
+  obj = Math.floor(Math.random() * 364); //random number between 0 and 14 * 26, or 364
   if(done.includes(obj) == false) { //if i haven't already done that number
     done.push(obj); //add the number to the list of numbers I've done
     if(o == 0) {
@@ -22,7 +22,7 @@ function vanish() {
     }
     ++i; //add one to the counter
   }
-  if(i == 251) { //if counter = number of shapes, reset everything and stop the repeating timer
+  if(i == 363) { //if counter = number of shapes, reset everything and stop the repeating timer
     clearInterval(q);
     o = 1 - o;
     done = [""];
